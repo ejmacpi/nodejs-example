0fdac7cb587df18a29f8566ef1f67e5c63b441b1
Eduardo Piza <70527521+ejmacpi@users.noreply.github.com>
2020-09-21 17:20:56 +0100

    build #1 | commit #70

--- dist/web_server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const port = 9002;
-const server = http.createServer((req, res) => {
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('nodejs web server\n');
-});
-server.listen(port, () => {
-  console.log(`Server running at localhost:${port}/`);
-});
